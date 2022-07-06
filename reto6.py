a008e07c76496ba0b782b1e11bf36209bf152f30
Julian Casallas <julian.casallasb@gmail.com>
2022-07-05 22:13:31 -0300

    Solucion reto 6 y reto 7

---
+++ reto6.py
@@ -0,0 +1,25 @@
+"""/*
+ * Reto #6
+ * INVIRTIENDO CADENAS
+ * Fecha publicación enunciado: 07/02/22
+ * Fecha publicación resolución: 14/02/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */"""
+def inverse(word):
+    for i in range(1,len(word)+1):
+        print(word[-i],end="")
+    print("\n")
+con = True 
+while con :
+    inverse(input("Ingrese la palabra que quiere invertir : "))
+    con = False if input("¿Oprima S si quiere continuar?").lower() == 's' else True 
